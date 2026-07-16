fae43993bef1a87def0253ee8fb820a9cf742413
mihirmithani <mihirmithani123@gmail.com>
2026-07-16 16:17:42 +0530

    Loops

---
+++ 04_loops/for.tcl
@@ -0,0 +1,1 @@
+for {puts "Start"; set i 0} {$i<2} {incr i; puts "I is after incr : $i"; } { puts "I inside first loop : $i" }
---
+++ 04_loops/while.tcl
@@ -0,0 +1,8 @@
+#while loop
+
+
+set x 0;
+while {$x < 6} {
+    puts "x is $x"
+    set x [expr $x +1]
+}
