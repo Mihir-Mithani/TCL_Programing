e13fb5b6c8376ad9375d69eec7f6391101a069f9
mihirmithani <mihirmithani123@gmail.com>
2026-07-16 16:47:25 +0530

    Associative Array

---
+++ 05_associative_array/as_array.tcl
@@ -0,0 +1,8 @@
+#Associative Array
+
+
+set Employee1(Name) "Mihir"
+set Employee1(Age) 21
+
+puts $Employee1(Name)
+puts $Employee1(Age)
---
+++ 05_associative_array/as_array1.tcl
@@ -0,0 +1,7 @@
+#Retriving indices from Associative Array
+
+
+set Employee1(Name) "Mihir"
+set Employee1(Age) 21
+
+puts [array names Employee1]
