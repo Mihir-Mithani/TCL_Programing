40cfb5da114d46ba341b8920a8409bcdaed28d08
mihirmithani <mihirmithani123@gmail.com>
2026-07-16 15:44:41 +0530

    Arithmetics Operations

---
+++ 03_arithmetic_ops/expr.tcl
@@ -0,0 +1,13 @@
+
+set a "5";
+set b "3";
+
+set c [expr "$a + $b"];
+set d [expr "$a - $b"];
+set e [expr "$a * $b"];
+set f [expr "$a / $b"];
+
+puts "addition of $a and $b is $c";
+puts "substraction of $a and $b is $d";
+puts "multiplication of $a and $b is $e";
+puts "divition of $a and $b is $f";
