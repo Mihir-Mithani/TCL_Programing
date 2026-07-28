52cd64cc19bcb157beecb28fec8fc707dffc4e8c
mihirmithani <mihirmithani123@gmail.com>
2026-07-28 17:14:04 +0530

    Assignment Task 10

--- assignment/10.tcl
+++ assignment/10.tcl
@@ -1,4 +1,11 @@
 #Author : Mihir Mithani
 #Assignment
+# 10. Create a list of 5 elements and print the entire list.
 
+set myList {apple banana cherry date elderberry}
 
+puts "The list is: $myList"
+puts "List elements:"
+foreach item $myList {
+    puts "  $item"
+}
