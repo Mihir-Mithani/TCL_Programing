0f748f383b3d3a6a057a82d166cafd7ee7a52ea6
mihirmithani <mihirmithani123@gmail.com>
2026-07-28 17:13:58 +0530

    Assignment Task 7

--- assignment/7.tcl
+++ assignment/7.tcl
@@ -1,4 +1,15 @@
 #Author : Mihir Mithani
 #Assignment
+# 7. Use an 'if-elseif-else' structure to determine whether a number is even or odd.
 
+set num 0
 
+puts "Enter a number: "
+flush stdout
+gets stdin num
+
+if {$num % 2 == 0} {
+    puts "$num is Even"
+} else {
+    puts "$num is Odd"
+}
