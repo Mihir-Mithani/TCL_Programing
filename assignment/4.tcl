493d5648f0c656ee8cdbb4ca57e1f0f9d4da672b
mihirmithani <mihirmithani123@gmail.com>
2026-07-28 17:13:52 +0530

    Assignment Task 4

--- assignment/4.tcl
+++ assignment/4.tcl
@@ -1,4 +1,14 @@
 #Author : Mihir Mithani
 #Assignment
 
+puts -nonewline "Enter first number : "
+flush stdout
+gets stdin num1
 
+puts -nonewline "Enter second number : "
+flush stdout
+gets stdin num2
+
+set sum [expr {$num1 + $num2}]
+
+puts "Addition of $num1 and $num2 is $sum"
