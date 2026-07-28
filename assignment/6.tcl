bdc167bf3849d0a249a2805eb086d67aa75b48ac
mihirmithani <mihirmithani123@gmail.com>
2026-07-28 17:13:56 +0530

    Assignment Task 6

--- assignment/6.tcl
+++ assignment/6.tcl
@@ -1,4 +1,17 @@
 #Author : Mihir Mithani
 #Assignment
+# 6. Write a script to check if a number is positive, negative, or zero.
 
+set num 0
 
+puts "Enter a number: "
+flush stdout
+gets stdin num
+
+if {$num > 0} {
+    puts "$num is Positive"
+} elseif {$num < 0} {
+    puts "$num is Negative"
+} else {
+    puts "$num is Zero"
+}
