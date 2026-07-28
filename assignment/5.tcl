ac9638e10f7a5cad07bab8e787961dbc926a0e68
mihirmithani <mihirmithani123@gmail.com>
2026-07-28 17:13:54 +0530

    Assignment Task 5

--- assignment/5.tcl
+++ assignment/5.tcl
@@ -1,4 +1,20 @@
 #Author : Mihir Mithani
 #Assignment
 
+puts -nonewline "Enter first number : "
+flush stdout
+gets stdin num1
 
+puts -nonewline "Enter second number : "
+flush stdout
+gets stdin num2
+
+set add [expr {$num1 + $num2}]
+set sub [expr {$num1 - $num2}]
+set mul [expr {$num1 * $num2}]
+set div [expr {$num1 / $num2}]
+
+puts "Addition of $num1 and $num2 is $add"
+puts "Substraction of $num1 and $num2 is $sub"
+puts "Multiplition of $num1 and $num2 is $mul"
+puts "Division of $num1 and $num2 is $div"
