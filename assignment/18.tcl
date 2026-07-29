f69524edf5bf15b907a1d4ef382eb41dad642043
mihirmithani <mihirmithani123@gmail.com>
2026-07-29 14:20:38 +0530

    Assignment Task 18

--- assignment/18.tcl
+++ assignment/18.tcl
@@ -1,4 +1,14 @@
 #Author : Mihir Mithani
 #Assignment
 
+#18. Write a script to create and call a procedure that returns the maximum of two numbers.
+ 
+proc max {a b} {
+    if {$a>$b} {
+        return $a
+    } else {
+        return $b
+    }
+}
 
+puts "Maximum of 10 and 20 is [max 10 20]"
