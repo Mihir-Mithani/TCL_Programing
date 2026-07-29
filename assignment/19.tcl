6af3517af5012a5c994a1874f5b2e314344ccdf0
mihirmithani <mihirmithani123@gmail.com>
2026-07-29 14:20:40 +0530

    Assignment Task 19

--- assignment/19.tcl
+++ assignment/19.tcl
@@ -1,4 +1,15 @@
 #Author : Mihir Mithani
 #Assignment
 
+#19. Open a file 'data.txt' and write three lines of text to it.
 
+set fp [open "data.txt" w+]
+puts $fp "Hello\nworld\nthis is mihir"
+close $fp
+
+set fp [open "data.txt" r]
+set file_data [read $fp]
+close $fp
+
+puts "--- File Contents ---"
+puts -nonewline $file_data
