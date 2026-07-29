a9cff761ab60775a9f140a8b1c730fe081d509c1
mihirmithani <mihirmithani123@gmail.com>
2026-07-29 14:20:44 +0530

    Assignment Task 21

--- assignment/21.tcl
+++ assignment/21.tcl
@@ -1,4 +1,14 @@
 #Author : Mihir Mithani
 #Assignment
 
+# 21. Count the number of lines in a file using Tcl. 
 
+set fp [open "data.txt" r]
+
+set count 0
+while { [gets $fp line] >= 0 } {
+    incr count
+}
+close $fp
+
+puts "Total Number of lines in data.txt : $count"
