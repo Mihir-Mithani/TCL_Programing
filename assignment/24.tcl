a7aed82b69cff63d34bb17fbcbfc3b7372f4a923
mihirmithani <mihirmithani123@gmail.com>
2026-07-29 14:20:50 +0530

    Assignment Task 24

--- assignment/24.tcl
+++ assignment/24.tcl
@@ -1,4 +1,8 @@
 #Author : Mihir Mithani
 #Assignment
 
+#24. Convert a string to uppercase (if manual logic required, use ASCII manipulation).
 
+set str "Mihir Mithani"
+
+puts "Original String : $str\nUppercase String : [string toupper $str]"
