c08e4cee3e6d548c64506da87c0c5eca523d3ec9
mihirmithani <mihirmithani123@gmail.com>
2026-07-29 14:20:36 +0530

    Assignment Task 17

--- assignment/17.tcl
+++ assignment/17.tcl
@@ -1,4 +1,10 @@
 #Author : Mihir Mithani
 #Assignment
 
+#17. Write a procedure to calculate the square of a number.
 
+proc square { a } {
+    return [expr $a*$a]
+}
+
+puts "Square of 10 is [square 10]"
