56f4cad77e8614dd460f5c543246e9b4df8ab8c6
mihirmithani <mihirmithani123@gmail.com>
2026-07-29 14:20:42 +0530

    Assignment Task 20

--- assignment/20.tcl
+++ assignment/20.tcl
@@ -1,4 +1,14 @@
 #Author : Mihir Mithani
 #Assignment
 
+#20. Read the contents of 'data.txt' and print them line by line.
 
+set fp [open "data.txt" r]
+
+
+
+while { [gets $fp line] >=0 } {
+    puts "Line : $line"
+}
+
+close $fp
