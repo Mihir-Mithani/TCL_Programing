3895c6f4d4d1bb00c93ab919c52d35e56e2feb21
mihirmithani <mihirmithani123@gmail.com>
2026-07-29 14:20:52 +0530

    Assignment Task 25

--- assignment/25.tcl
+++ assignment/25.tcl
@@ -1,4 +1,13 @@
 #Author : Mihir Mithani
 #Assignment
 
+#25. Compare two strings and print whether they are equal.
 
+set s1 "Mihir Mithani"
+set s2 "Mihir Mithani"
+
+if {$s1==$s2} {
+    puts "Both Strings Are Equal"
+} else {
+    puts "Both Strings Are Not Equal"
+}
