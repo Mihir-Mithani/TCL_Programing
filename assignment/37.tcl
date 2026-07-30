a572f2f19dbfd5c9209c4f8b107621c542f93108
mihirmithani <mihirmithani123@gmail.com>
2026-07-30 14:35:56 +0530

    Assignment Task 37

--- assignment/37.tcl
+++ assignment/37.tcl
@@ -1,4 +1,19 @@
 #Author : Mihir Mithani
 #Assignment
 
+set emails {
+    "mihir.mithani@example.com"
+    "invalid-email.com"
+    "test.user+alias@domain.co.in"
+    "missing@domain"
+}
 
+set email_pattern {^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$}
+
+foreach email $emails {
+    if {[regexp $email_pattern $email]} {
+        puts "Valid   : $email"
+    } else {
+        puts "Invalid : $email"
+    }
+}
