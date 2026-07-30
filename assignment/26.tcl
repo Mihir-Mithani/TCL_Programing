cdeea7923be6c5098e84e96d091f74d7f270ec61
mihirmithani <mihirmithani123@gmail.com>
2026-07-30 12:04:49 +0530

    Assignment Task 26

--- assignment/26.tcl
+++ assignment/26.tcl
@@ -1,4 +1,17 @@
 #Author : Mihir Mithani
 #Assignment
 
+set mystr "My name is mihir mithani"
 
+puts "Current String : $mystr"
+
+set lst [split $mystr " "]
+
+set len [llength $lst]
+
+set i 0
+
+while {$i<$len} {
+    puts "Index $i : [lindex $lst $i]"
+    incr i
+}
