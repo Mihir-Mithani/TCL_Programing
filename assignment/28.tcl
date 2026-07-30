2fd8f81f6f29f46c0c04d312af35e6ebcc58e8a2
mihirmithani <mihirmithani123@gmail.com>
2026-07-30 12:04:53 +0530

    Assignment Task 28

--- assignment/28.tcl
+++ assignment/28.tcl
@@ -2,3 +2,16 @@
 #Assignment
 
 
+array set student {
+    name "Mihir"
+    age 22
+    major "Engineering"
+    cgpa 9.3
+    university "Marwadi University"
+}
+
+
+puts "Keys in the array : "
+foreach key [array names student] {
+    puts $key
+}
