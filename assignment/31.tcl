5e9a22d6d9027f31a0432dd27799b80976639786
mihirmithani <mihirmithani123@gmail.com>
2026-07-30 12:04:58 +0530

    Assignment Task 31

--- assignment/31.tcl
+++ assignment/31.tcl
@@ -2,3 +2,24 @@
 #Assignment
 
 
+array set student {
+    name "Mihir"
+    age 22
+    major "Engineering"
+    cgpa 9.3
+    university "Marwadi University"
+}
+
+
+puts "Old Array : "
+foreach key [array names student] {
+    puts "student($key) : $student($key)"
+}
+
+unset student(cgpa)
+
+puts "\nNew Array : "
+foreach key [array names student] {
+    puts "student($key) : $student($key)"
+}
+
