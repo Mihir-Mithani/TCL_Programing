0c41cf96c5a69b65cc00b85181c793f273847e84
mihirmithani <mihirmithani123@gmail.com>
2026-07-30 14:35:58 +0530

    Assignment Task 38

--- assignment/38.tcl
+++ assignment/38.tcl
@@ -1,4 +1,14 @@
 #Author : Mihir Mithani
 #Assignment
 
+set str "My name is mihir mithani"
 
+set rev ""
+
+set len [string length $str]
+
+for { set i [expr $len-1] } { $i>=0 } { incr i -1 } {
+    append rev [string index $str $i]
+}
+
+puts "Original String : $str\nReversed String : $rev"
