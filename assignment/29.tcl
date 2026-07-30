545ad383f1a5a0681008adce75e34df5a68809c4
mihirmithani <mihirmithani123@gmail.com>
2026-07-30 12:04:55 +0530

    Assignment Task 29

--- assignment/29.tcl
+++ assignment/29.tcl
@@ -1,4 +1,12 @@
-#Author : Mihir Mithani
+#Author : Mihir Mithani 
 #Assignment
 
+set arr(0) "My"
+set arr(1) "Name"
+set arr(2) "is"
+set arr(3) "Mihir"
+set arr(4) "Mithani"
 
+foreach value [lsort -integer [array names arr]] {
+    puts "arr($value) : $arr($value)"
+}
