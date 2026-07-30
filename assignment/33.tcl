ad9062e57817b726e04b34807c1e242bbc9222fd
mihirmithani <mihirmithani123@gmail.com>
2026-07-30 12:05:02 +0530

    Assignment Task 33

--- assignment/33.tcl
+++ assignment/33.tcl
@@ -1,4 +1,12 @@
 #Author : Mihir Mithani
 #Assignment
 
+proc leap { a } {
+    if {[expr $a % 4] == 0} {
+        puts "$a is a leap year"
+    } else {
+        puts "$a is not a leap year"
+    }
+}
 
+leap 2026
