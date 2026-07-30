8994d420a85c67e8f7ce899dea5f68a5a6538821
mihirmithani <mihirmithani123@gmail.com>
2026-07-30 12:05:05 +0530

    Assignment Task 35

--- assignment/35.tcl
+++ assignment/35.tcl
@@ -1,4 +1,21 @@
 #Author : Mihir Mithani
 #Assignment
 
+proc cal {a b choice} {
+    switch $choice {
+        "+" {
+            return [expr $a+$b]
+        }
+        "-" {
+            return [expr $a-$b]
+        }
+        "*" {
+            return [expr $a*$b]
+        }
+        "/" {
+            return [expr $a/$b]
+        }
+    }
+}
 
+puts "Multiplication of 10 and 20 is : [cal 10 20 "*"] "
