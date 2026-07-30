ca0e66aa81f3818a0e81856739d11b008d5bb65a
mihirmithani <mihirmithani123@gmail.com>
2026-07-30 12:05:04 +0530

    Assignment Task 34

--- assignment/34.tcl
+++ assignment/34.tcl
@@ -1,4 +1,21 @@
 #Author : Mihir Mithani
 #Assignment
 
+puts -nonewline "Enter your username: "
+flush stdout
 
+
+set username [gets stdin]
+
+if {$username == "Mihir"} {
+    puts -nonewline "Enter Password for $username : "
+    flush stdout
+    set password [gets stdin]
+    if {$password==12345678} {
+        puts "\n-----------------\n--Login Success--\n-----------------\n\nWelcome $username"
+    } else {
+        puts "Incorrect Password!!!!!!\nTry again!!!!!"
+    }
+} else {
+    puts "No User Exists as $username....try again..."
+}
