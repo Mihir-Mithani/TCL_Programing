ded54dbf0db584c5643f7a7f86188e274df2ea4a
mihirmithani <mihirmithani123@gmail.com>
2026-07-30 14:36:01 +0530

    Assignment Task 40

--- assignment/40.tcl
+++ assignment/40.tcl
@@ -1,4 +1,21 @@
 #Author : Mihir Mithani
 #Assignment
 
+proc fib {n} {
+    if {$n <= 0} {
+        return 0
+    } elseif {$n == 1} {
+        return 1
+    } else {
+        return [expr {[fib [expr {$n - 1}]] + [fib [expr {$n - 2}]]}]
+    }
+}
 
+set n 10
+set series {}
+
+for {set i 0} {$i < $n} {incr i} {
+    lappend series [fib $i]
+}
+
+puts "Fibonacci series ($n terms): $series"
