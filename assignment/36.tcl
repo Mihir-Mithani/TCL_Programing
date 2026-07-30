2c8fcffc9c15ad14b2a094cf4680879f38559754
mihirmithani <mihirmithani123@gmail.com>
2026-07-30 14:35:53 +0530

    Assignment Task 36

--- assignment/36.tcl
+++ assignment/36.tcl
@@ -1,4 +1,10 @@
 #Author : Mihir Mithani
 #Assignment
 
+set mystr "My order ID is 4589 and the price is 99.50"
 
+puts "Original String : $mystr"
+
+set digits [regexp -all -inline {\d+} $mystr]
+
+puts "Digits found    : $digits"
