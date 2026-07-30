8e2cd5facf46d3023d3a317603d564ad1867dcd0
mihirmithani <mihirmithani123@gmail.com>
2026-07-30 14:35:59 +0530

    Assignment Task 39

--- assignment/39.tcl
+++ assignment/39.tcl
@@ -1,4 +1,12 @@
 #Author : Mihir Mithani
 #Assignment
 
+proc fact {a} {
+    set fac 1
+    for {set i 1} {$i<$a} {incr i 1} {
+        set fac [expr $fac*$i]
+    }
+    return $fac
+}
 
+puts "Factorial of 10 is [fact 10]"
