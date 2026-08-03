089bf161bb77d7ae26ba295653a520988e39ddc3
mihirmithani <mihirmithani123@gmail.com>
2026-08-03 15:54:24 +0530

    Assignment Task 47

--- assignment/47.tcl
+++ assignment/47.tcl
@@ -1,4 +1,10 @@
 #Author : Mihir Mithani
 #Assignment
 
+set lst {1 4 6 8 24 22 16 17 29 30 45 25}
 
+puts "Original List : $lst"
+
+set lst2 [lsort -integer -decreasing $lst]
+
+puts "Decending List : $lst2"
