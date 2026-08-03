79504a330622c7512a87f88325c2e5cff732a8e6
mihirmithani <mihirmithani123@gmail.com>
2026-08-03 15:54:28 +0530

    Assignment Task 49

--- assignment/49.tcl
+++ assignment/49.tcl
@@ -1,4 +1,18 @@
 #Author : Mihir Mithani
 #Assignment
 
+proc evens {lst} {
+    set lst2 {}
+    foreach value $lst {
+        if { [expr {$value%2}]==0 } {
+            lappend lst2 $value
+        }
+    }
+    return $lst2
+}
 
+set lst {1 4 6 8 24 22 16 17 29 30 45 25}
+
+
+
+puts "Original List : $lst\nEven Number : [evens $lst]"
