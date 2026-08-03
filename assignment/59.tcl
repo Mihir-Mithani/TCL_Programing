92d4f0af2f7ca4f157aeebd22944c17e18c8ac7d
mihirmithani <mihirmithani123@gmail.com>
2026-08-03 17:14:53 +0530

    Assignment Task 59

--- assignment/59.tcl
+++ assignment/59.tcl
@@ -1,4 +1,18 @@
 #Author : Mihir Mithani
 #Assignment
 
+proc longest_word {lst} {
+    set word ""
+    set length 0
+    foreach str $lst {
+        if {[string length $str]>$length} {
+            set word $str
+            set length [string length $str]
+        }
+    }
+    return $word
+}
 
+set lst_str {My name is mihir mithani}
+
+puts "The Longest Word in $lst_str is : [longest_word $lst_str]"
