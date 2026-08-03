7fb2af7f7ca658966845c1383c0bd968e30e4a65
mihirmithani <mihirmithani123@gmail.com>
2026-08-03 15:54:29 +0530

    Assignment Task 50

--- assignment/50.tcl
+++ assignment/50.tcl
@@ -1,4 +1,11 @@
 #Author : Mihir Mithani
 #Assignment
 
+set log [open "output.log" w+]
+puts $log "--- Log Session Started ---"
+puts $log "Task executed successfully by Mihir Mithani."
+puts $log "Timestamp: [clock format [clock seconds]]"
+puts $log "--- Log Session Ended ---"
+close $log
 
+puts "Logs Saved to Output.log"
