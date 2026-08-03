11710d2ad0d2cca8ff0e8f02e22301c854343728
mihirmithani <mihirmithani123@gmail.com>
2026-08-03 17:14:55 +0530

    Assignment Task 60

--- assignment/60.tcl
+++ assignment/60.tcl
@@ -2,3 +2,12 @@
 #Assignment
 
 
+set str "My name is Mihir Mithani"
+
+set first [string index $str 0]
+
+if {[string is upper $first]} {
+    puts "String Starts With UpperCase"
+} else {
+    puts "String Does Not Start With UpperCase"
+}
