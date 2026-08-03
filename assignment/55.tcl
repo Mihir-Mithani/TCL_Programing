aff98fde807f3d888ecd232b65846864d9ad1ad3
mihirmithani <mihirmithani123@gmail.com>
2026-08-03 15:54:39 +0530

    Assignment Task 55

--- assignment/55.tcl
+++ assignment/55.tcl
@@ -1,4 +1,17 @@
 #Author : Mihir Mithani
 #Assignment
 
+set num(a) 10
+set num(b) 20
+set num(c) 15
 
+set max 0
+set maxkey -1
+foreach key [array names num] {
+    if {$num($key)>$max} {
+        set max $num($key)
+        set maxkey $key
+    }
+}
+
+puts "Maximum value found at $maxkey with value $max"
