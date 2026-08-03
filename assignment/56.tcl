7fa9fbfb14e54e9a151d2022f7f0a17170a59495
mihirmithani <mihirmithani123@gmail.com>
2026-08-03 15:54:41 +0530

    Assignment Task 56

--- assignment/56.tcl
+++ assignment/56.tcl
@@ -1,4 +1,15 @@
 #Author : Mihir Mithani
 #Assignment
 
+# Define a global variable
+set counter 10
 
+proc increment_counter {} {
+    global counter
+    set counter [expr {$counter + 5}]
+    puts "Inside procedure, counter = $counter"
+}
+
+puts "Before procedure call, counter = $counter"
+increment_counter
+puts "After procedure call, counter = $counter"
