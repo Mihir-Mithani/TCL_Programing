86c6459c80b283a27b3c5b2e1dba509d6e51200d
mihirmithani <mihirmithani123@gmail.com>
2026-08-03 15:54:22 +0530

    Assignment Task 46

--- assignment/46.tcl
+++ assignment/46.tcl
@@ -1,4 +1,14 @@
 #Author : Mihir Mithani
 #Assignment
 
+set lst { {a b b} {d e f} {g h i} }
+
+set index 0
+
+puts "Original  List : $lst"
+
+foreach sub $lst {
+    puts "Sublist $index : $sub"
+    incr index
+}
 
