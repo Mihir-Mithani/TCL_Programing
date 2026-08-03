7ae45e103703913d3400b17627b9bc9722feb51f
mihirmithani <mihirmithani123@gmail.com>
2026-08-03 15:54:31 +0530

    Assignment Task 51

--- assignment/51.tcl
+++ assignment/51.tcl
@@ -1,4 +1,26 @@
 #Author : Mihir Mithani
 #Assignment
 
+set lst {name age sub1 sub2 sub3}
+array set student {}
 
+foreach field $lst {
+    puts -nonewline "Enter $field : "
+    flush stdout
+    gets stdin student($field)
+}
+
+set total 0
+for {set i 1} {$i < 4} {incr i} {
+    set total [expr {$total + $student(sub$i)}]
+}
+puts "Total : $total"
+
+set average [expr {$total / 3.0}]
+puts "Average : $average"
+
+puts "\n==========Student Details==========\n"
+
+foreach value [array names student] {
+    puts "$value : $student($value)"
+}
