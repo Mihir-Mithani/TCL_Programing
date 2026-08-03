228a94449f73a0f49134f3d8739475a305404f0f
mihirmithani <mihirmithani123@gmail.com>
2026-08-03 15:54:37 +0530

    Assignment Task 54

--- assignment/54.tcl
+++ assignment/54.tcl
@@ -1,4 +1,20 @@
 #Author : Mihir Mithani
 #Assignment
 
+set lst {1 2 3 4 4 5 6 6 7 8 9 9 10}
 
+foreach value $lst {
+    incr freq($value)
+}
+
+puts "=====Frequency Map : =====\n" 
+foreach value [lsort -integer [array names freq]] {
+    puts "$value : $freq($value)"
+}
+puts "\n=====Duplicate Elements : =====\n"
+
+foreach value [array names freq] {
+    if {$freq($value)>1} {
+        puts "$value"
+    }
+}
