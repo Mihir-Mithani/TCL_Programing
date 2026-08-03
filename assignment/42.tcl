6ed90a7fbcc0fcae3d6e4cbeb424e640fe7e393c
mihirmithani <mihirmithani123@gmail.com>
2026-08-03 15:54:14 +0530

    Assignment Task 42

--- assignment/42.tcl
+++ assignment/42.tcl
@@ -1,4 +1,14 @@
 #Author : Mihir Mithani
 #Assignment
 
+set num3 10
+set num4 0
 
+puts "Attempting division: $num3 / $num4"
+set status [catch {expr {$num3 / $num4}} result]
+
+if {$status == 0} {
+    puts "Success! Result: $result"
+} else {
+    puts "Error caught successfully: $result"
+}
