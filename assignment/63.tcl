24f0fa3a4cd1ea9ed395048ff967abfa5a0f1537
mihirmithani <mihirmithani123@gmail.com>
2026-08-04 10:23:36 +0530

    Assignment Task 63

--- assignment/63.tcl
+++ assignment/63.tcl
@@ -2,3 +2,8 @@
 #Assignment
 
 
+set text "You can reach us at +91-9876543210 or our support desk at +91-9123456789. Invalid numbers include +91-12345 and 9876543210."
+
+set phone_number [regexp -all -inline  {\+91-\d{10}} $text]
+
+puts "Phone numbers in : \"$text\" are \n\n$phone_number"
