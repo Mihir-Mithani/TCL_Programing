ce0f3a4b8b9ee35ac06a0db71ad72645823abbc5
mihirmithani <mihirmithani123@gmail.com>
2026-08-04 10:23:45 +0530

    Assignment Task 68

--- assignment/68.tcl
+++ assignment/68.tcl
@@ -2,3 +2,20 @@
 #Assignment
 
 
+set lst {1 2 3 4 5 6 7 8 9 0}
+set lst2 {1 3 5 7 9}
+
+puts "Elements that are in \"$lst\" and not in \"$lst2\" are : \n"
+
+foreach value $lst {
+	set flag 0
+	foreach val $lst2 {
+		if {$value==$val} {
+			set flag 1
+			break
+		}
+	}
+	if {$flag==0} {
+		puts $value
+	}
+}
