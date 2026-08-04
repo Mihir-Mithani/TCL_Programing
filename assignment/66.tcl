5450a3aa4596f9f06a306413e066271c05e1aba7
mihirmithani <mihirmithani123@gmail.com>
2026-08-04 10:23:41 +0530

    Assignment Task 66

--- assignment/66.tcl
+++ assignment/66.tcl
@@ -1,4 +1,19 @@
 #Author : Mihir Mithani
 #Assignment
 
+proc remove_duplicate_lines {input_file output_file} {
+    set fin [open $input_file r]
+    set fout [open $output_file w+]
+    set unique_lines [dict create]
+    while {[gets $fin line] != -1} {
+        dict set unique_lines $line 1
+    }
+    close $fin
+    foreach line [dict keys $unique_lines] {
+        puts $fout $line
+    }
+    close $fout
+    puts "Successfully removed duplicate lines from '$input_file'. Output saved to '$output_file'."
+}
 
+remove_duplicate_lines "git_log.txt" "git_log_removed.txt"
