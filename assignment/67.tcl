6f841dc1320c3269adc1e43823e0d8725bdb6d97
mihirmithani <mihirmithani123@gmail.com>
2026-08-04 10:23:43 +0530

    Assignment Task 67

--- assignment/67.tcl
+++ assignment/67.tcl
@@ -1,4 +1,36 @@
 #Author : Mihir Mithani
 #Assignment
 
-
+proc compare_files {file1_path file2_path} {
+    set f1 [open $file1_path r]
+    set f2 [open $file2_path r]
+    set line_num 1
+    while {1} {
+        set has1 [gets $f1 line1]
+        set has2 [gets $f2 line2]
+        if {$has1 < 0 && $has2 < 0} {
+            break
+        }
+        if {$has1 < 0 || $has2 < 0 || $line1 ne $line2} {
+            puts "Difference found at Line $line_num:"
+            
+            if {$has1 >= 0} {
+                puts "  $file1_path: $line1"
+            } else {
+                puts "  $file1_path: <EOF>"
+            }
+            
+            if {$has2 >= 0} {
+                puts "  $file2_path: $line2"
+            } else {
+                puts "  $file2_path: <EOF>"
+            }
+            puts ""
+        }
+        incr line_num
+    }
+    close $f1
+    close $f2
+    puts "File comparison completed."
+}
+compare_files "git_log.txt" "git_log.log"
