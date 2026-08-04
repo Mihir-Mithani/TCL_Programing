7b09f205090021c48271b0bb3f34787bf238d882
mihirmithani <mihirmithani123@gmail.com>
2026-08-04 10:23:34 +0530

    Assignment Task 62

--- assignment/62.tcl
+++ assignment/62.tcl
@@ -1,4 +1,10 @@
 #Author : Mihir Mithani
 #Assignment
 
+set str "Hello, World!"
 
+if {[regexp {[ ^a-zA-Z0-9 ]} $str]} {
+	puts "String Contains Special Character"
+} else {
+	puts "String Does Not Contains Special Character"
+}
