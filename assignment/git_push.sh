32180d6df0193d73b83e2897156aadc5bbb3142a
mihirmithani <mihirmithani123@gmail.com>
2026-08-04 10:24:54 +0530

    Utility Files

--- assignment/git_push.sh
+++ assignment/git_push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-for i in {41..57}
+for i in {61..68}
 do
     git add $i.tcl
     git commit -m "Assignment Task $i"
--- assignment/run_file.sh
+++ assignment/run_file.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in {52..55}
+for i in {61..68}
 do
     bat $i.tcl
     tclsh $i.tcl
