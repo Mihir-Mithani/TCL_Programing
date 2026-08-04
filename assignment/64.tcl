f1b8deb9324217d451a5a95f41a0446357e7c933
mihirmithani <mihirmithani123@gmail.com>
2026-08-04 10:23:37 +0530

    Assignment Task 64

--- assignment/64.tcl
+++ assignment/64.tcl
@@ -2,3 +2,12 @@
 #Assignment
 
 
+set file [open "git_log.txt" r]
+
+set line_num 1
+
+while {[gets $file line] != -1} {
+	set word_count [llength $line]
+    puts "Line $line_num: $word_count words"
+    incr line_num
+}
