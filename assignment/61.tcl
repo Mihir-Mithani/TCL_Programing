1034246b5e8e2974de647fde10341aa20c463632
mihirmithani <mihirmithani123@gmail.com>
2026-08-04 10:23:32 +0530

    Assignment Task 61

--- assignment/61.tcl
+++ assignment/61.tcl
@@ -1,4 +1,10 @@
 #Author : Mihir Mithani
 #Assignment
 
+set str "1234567890"
 
+if {[string is digit $str] && [string length $str]>0} {
+	puts "String Only Contains Digits"
+} else {
+	puts "String Does Not Only Contains Digits"
+}
